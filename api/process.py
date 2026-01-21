72f50ecba458bc7eedce8a5948aa69ee484aa004
abhishek84-62 <proabhishek662@gmail.com>
2026-01-21 19:42:47 +0530

    Create process.py

---
+++ api/process.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(json.dumps({
+            "message": "✅ Website live & backend working!"
+        }).encode())
